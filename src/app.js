7ce3a9e30991c9c7673cd1370e9f05be4a94a26f
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-03-13 21:38:37 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let cartas = ["♦", "♥", "♠", "♣"];
+let numeros = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let picas = cartas[Math.floor(Math.random() * cartas.length)];
+  let num = numeros[Math.floor(Math.random() * numeros.length)];
+  document.getElementById("prueba").innerHTML = picas;
+  document.getElementById("prueba2").innerHTML = picas;
+  document.getElementById("numero").innerHTML = num;
 };
